72689cbad84fcfca144de9d6f5ef1918b9463a21
vrothx00 <vrothx00@gmail.com>
2023-02-10 14:50:05 +0100

    Added html tags cheat sheet

--- isOdd.js
+++
@@ -1,8 +0,0 @@
-function isOdd(num) {
-	// your code here!
-	return num % 2 !== 0;
-  }
-  
-  console.log(isOdd(2)); // => false
-  console.log(isOdd(5)); // => true
-  console.log(isOdd(-17)); // => true
