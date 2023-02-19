b0eb025798fabd6ead1cc3b327904c0ce990dbb5
vrothx00 <vrothx00@gmail.com>
2023-02-19 12:15:14 +0100

    Update no stand js project

--- no-standard-js-practice/problems.js
+++ no-standard-js-practice/problems.js
@@ -3,7 +3,7 @@ function oddIndices(arr) {
     // Your code here
     let newArr = [];
     for(let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 != 0) {
+        if (arr[i] % 2 == 0) {
             newArr.push(arr[i]);
         }
     }
@@ -13,6 +13,13 @@ function oddIndices(arr) {
 function oddReverse(arr) {
     // Return an array containing all the odd indices starting from the end
     // Your code here
+    let newArr = [];
+    for(let i = arr.length; i > 0; i--) {
+        if (arr[i] % 2 == 0) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
 
 function secondPower(arr) {
@@ -29,12 +36,24 @@ function firstHalf(arr) {
     // Return an array containing the first half of an array
     // Include middle index on odd length arr
     // Your code here
+    let newArr = []
+    max = Math.round(arr.length / 2)
+    for(let i = 0; i < max; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
 }
 
 function secondHalf(arr) {
     // Return an array containing the second half of an array
     // Exclude middle index on odd length arr
     // Your code here
+    let newArr = []
+    max = Math.round(arr.length / 2)
+    for(let i = max; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
 }
 
 module.exports = {
